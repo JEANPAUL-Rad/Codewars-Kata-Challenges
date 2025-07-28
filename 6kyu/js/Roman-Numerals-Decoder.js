2597203bb95c338e882a51cca04b8d4da84d1ce3
Jean Paul Iradukunda <iradukundajeanpaul60@gmail.com>
2025-07-28 14:42:58 +0200

    valid phone number

--- 6kyu/js/Roman-Numerals-Decoder.js
+++ 6kyu/js/Roman-Numerals-Decoder.js
@@ -27,4 +27,32 @@
 // Courtesy of rosettacode.org
 
 
-// 
+// solution
+
+function solution(roman) {
+    const values = {
+        I: 1, V: 5, X: 10,
+        L: 50, C: 100,
+        D: 500, M: 1000
+    };
+
+    let total = 0;
+
+    for (let i = 0; i < roman.length; i++) {
+        const current = values[roman[i]];
+        const next = values[roman[i + 1]];
+
+        if (current < next) {
+            total += next - current;
+            i++;
+        } else {
+            total += current;
+        }
+    }
+
+    return total;
+}
+
+console.log(solution("MCMXC")); // 1990
+console.log(solution("MMVIII")); // 2008
+console.log(solution("MDCLXVI")); // 1666
